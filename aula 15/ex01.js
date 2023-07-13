f6e0a3256e1821673f0822a7d005043be3844d00
valmirleite <129377852+Valmirleite@users.noreply.github.com>
2023-07-13 10:17:42 +0100

    atualização

---
+++ aula 15/ex01.js
@@ -0,0 +1,6 @@
+let num = [7,3,9,4,2]
+
+num.sort() //para organizar em ordem crescente
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
